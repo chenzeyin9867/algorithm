e22b5028439105c8552a20db4103985fdab603a6
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-09 13:58:04 +0800

    89 ez

---
+++ 2021_7/maxDepth.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+
+        return 1 + l if l > r else 1 + r
