9eb51d7a381117dd9d8e171bfccf2df546054567
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-19 20:10:53 +0800

    XOR

---
+++ 2021_8/singleNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+        for (auto e: nums) ret ^= e;
+        return ret;
+    }
+};
+
