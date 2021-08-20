dcac1eb0e89a3ed14f4ee9b9b3d00b9b271f240c
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-20 16:55:07 +0800

    include some general head files

---
+++ 2021_8/head.h
@@ -0,0 +1,13 @@
+// include some necessary heads for convinient
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <memory>
+#include <algorithm>
+using namespace std;
+#endif
