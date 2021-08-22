3fa5a1908af60bbb8512c96b275b87ce88bb9dba
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-22 15:30:02 +0800

    loop

---
+++ 2021_8/convertToTitle.cpp
@@ -0,0 +1,20 @@
+#include "head.h"
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            ans += a0 - 1 + 'A';
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.convertToTitle(52) << endl;
+    return 0;
+}
