d960fe0c73219ade0df57cf54c2fcad024b62e20
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-23 15:08:35 +0800

    simplify

---
+++ 2021_8/countPrimes.cpp
@@ -0,0 +1,27 @@
+#include "head.h"
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> isPrime(n, 1);
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i]) {
+                ans += 1;
+                if ((long long)i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = 0;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+int main(){
+    Solution s;
+    cout << s.countPrimes(10000) << endl;
+    return 0;
+}
