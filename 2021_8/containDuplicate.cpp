4f3323860b700b4bdb51d910b5a04ceb17c9a2cf
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-24 11:32:18 +0800

    using set

---
+++ 2021_8/containDuplicate.cpp
@@ -0,0 +1,15 @@
+#include "head.h"
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        set<int> s;
+        for(auto i:nums){
+            if(s.find(i)!=s.end()){
+                return true;
+            }
+            s.emplace(i);
+        }
+        return false;
+    }
+};
