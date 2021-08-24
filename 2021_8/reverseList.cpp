cb9dc6978d2b43c123209d9758310cae8526979b
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-24 11:28:00 +0800

    notice we should set the first node->next to null

---
+++ 2021_8/reverseList.cpp
@@ -0,0 +1,34 @@
+#include "head.h"
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return nullptr;
+        if(!head->next) return head;
+        ListNode* cur , *prev;
+        int a, b;
+        prev = head;
+        cur = head->next;
+        head->next = nullptr;
+        while(cur){
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;    
+        }
+        return prev;
+    }
+};
+
+int main(){
+    Solution s;
+    ListNode *head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode *ret = s.reverseList(head);
+    while(ret){
+        cout << ret->val << " ";
+        ret = ret->next;
+
+    }
+    cout << endl;
+
+    return 0;
+}
