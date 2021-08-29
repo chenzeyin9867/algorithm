607bfd9ff8d37267c8c7060d8d5804be4a0643f5
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-08-29 13:21:15 +0800

    move

---
+++ 2021_8/moveZeroes.cpp
@@ -0,0 +1,15 @@
+#include "head.h"
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0;
+        while (right < n) {
+            if (nums[right]) {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
+
