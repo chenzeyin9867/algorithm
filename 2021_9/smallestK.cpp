dfd8519d78dfe20746c1f47714a9911a17567d17
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-09-03 11:15:09 +0800

    priority queue

---
+++ 2021_9/smallestK.cpp
@@ -0,0 +1,26 @@
+#include <head.h>
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        vector<int> vec(k, 0);
+        if (k == 0) { // 排除 0 的情况
+            return vec;
+        }
+        priority_queue<int> Q;
+        for (int i = 0; i < k; ++i) {
+            Q.push(arr[i]);
+        }
+        for (int i = k; i < (int)arr.size(); ++i) {
+            if (Q.top() > arr[i]) {
+                Q.pop();
+                Q.push(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = Q.top();
+            Q.pop();
+        }
+        return vec;
+    }
+};
+
