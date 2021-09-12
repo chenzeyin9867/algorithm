0555ede5226c304bfbde9edec216986ed1cf18f4
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-09-12 12:55:00 +0800

    iter

---
+++ 2021_9/checkPerfectNumber.cpp
@@ -0,0 +1,17 @@
+#include "head.h"
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num == 1) return false;
+        int sum = 1;
+        for(int i = 2; i <= sqrt(num); ++i){
+            if(num % i == 0){
+                sum += i;
+                if(num != (i * i)){
+                    sum += num/i;
+                }
+            }
+        }
+        return sum == num;
+    }
+};
