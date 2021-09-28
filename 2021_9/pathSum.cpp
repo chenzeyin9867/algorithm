a552ded494814f0d4442cc27b569795a063e131c
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-09-28 11:15:41 +0800

    using recursive & recursive

---
+++ 2021_9/pathSum.cpp
@@ -0,0 +1,30 @@
+#include "head.h"
+class Solution {
+public:
+    int rootSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return 0;
+        }
+
+        int ret = 0;
+        if (root->val == targetSum) {
+            ret++;
+        } 
+
+        ret += rootSum(root->left, targetSum - root->val);
+        ret += rootSum(root->right, targetSum - root->val);
+        return ret;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return 0;
+        }
+        
+        int ret = rootSum(root, targetSum);
+        ret += pathSum(root->left, targetSum);
+        ret += pathSum(root->right, targetSum);
+        return ret;
+    }
+};
+
