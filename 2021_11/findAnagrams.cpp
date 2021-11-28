92e8404a22ff7ef29b87d2f32f107ae6769c1926
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-11-28 13:13:20 +0800

    using sliding window

---
+++ 2021_11/findAnagrams.cpp
@@ -0,0 +1,69 @@
+/**
+ * @brief       ret the list of indexs of anagrams
+ * @author 		chenzeyin (chenzeyin9867@buaa.edu.cn)
+ * @date		2021-11-28
+ */
+#include "head.h"
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int sLen = s.size(), pLen = p.size();
+
+        if (sLen < pLen) {
+            return vector<int>();
+        }
+
+        vector<int> ans;
+        vector<int> count(26);
+        for (int i = 0; i < pLen; ++i) {
+            ++count[s[i] - 'a'];
+            --count[p[i] - 'a'];
+        }
+
+        int differ = 0;
+        for (int j = 0; j < 26; ++j) {
+            if (count[j] != 0) {
+                ++differ;
+            }
+        }
+
+        if (differ == 0) {
+            ans.emplace_back(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            if (count[s[i] - 'a'] == 1) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s[i] - 'a'] == 0) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            --count[s[i] - 'a'];
+
+            if (count[s[i + pLen] - 'a'] == -1) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s[i + pLen] - 'a'] == 0) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            ++count[s[i + pLen] - 'a'];
+            
+            if (differ == 0) {
+                ans.emplace_back(i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+int main()
+{
+    Solution s;
+    string str = "cbaebabacd";
+    string p = "abc";
+    vector<int> ret = s.findAnagrams(str, p);
+    printVector(ret);
+    return 0;
+}
