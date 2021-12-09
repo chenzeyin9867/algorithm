f5de32722f7492ef4cd9d163383302612890bd04
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-12-09 11:31:20 +0800

    use hash to pre

--- 2021_12/subarraySum.cpp
+++ 2021_12/subarraySum.cpp
@@ -2,18 +2,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-        for (int start = 0; start < nums.size(); ++start) {
-            int sum = 0;
-            for (int end = start; end >= 0; --end) {
-                sum += nums[end];
-                if (sum == k) {
-                    count++;
-                }
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0, pre = 0;
+        for (auto& x:nums) {
+            pre += x;
+            if (mp.find(pre - k) != mp.end()) {
+                count += mp[pre - k];
             }
+            mp[pre]++;
         }
         return count;
     }
 };
-
-
