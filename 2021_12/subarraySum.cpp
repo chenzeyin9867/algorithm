f0eb8df4ac9e86c2d55c35f9b9b90e94791451ab
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-12-09 11:30:11 +0800

    loop

---
+++ 2021_12/subarraySum.cpp
@@ -0,0 +1,19 @@
+#include "head.h"
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.size(); ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
