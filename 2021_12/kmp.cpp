c32dcc91416fd12575de2de06f9632c983fefc3a
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2021-12-22 14:04:20 +0800

    kmp implementation

--- 2021_12/kmp.cpp
+++ 2021_12/kmp.cpp
@@ -9,26 +9,47 @@
 #include "head.h"
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* head = new ListNode(0), *cur = head;
-        ListNode* h1 = l1;
-        ListNode* h2 = l2;
-        while(h1 && h2){
-            if(h1->val < h2->val){
-                cur->next = h1;
-                cur = cur->next;
-                h1 = h1->next;
+    int strStr(string haystack, string needle) {
+        // implement a kmp method
+        if(needle.size() == 0) return 0;
+        if(needle.size() > haystack.size()) return -1;
+        // create the next() array
+        vector<int> next_arr;
+        next_arr.push_back(0);
+        int i = 1;
+        int now = 0;
+        while(i < needle.size()){
+            if(needle[now] == needle[i]){
+                ++i;
+                ++now;
+                next_arr.push_back(now);
+            }else if(now > 0){
+                now = next_arr[now-1];
             }else{
-                cur->next = h2;
-                cur = cur->next;
-                h2 = h2->next;
+                next_arr.push_back(0);
+                ++i;
             }
         }
-        if(h1){
-            cur->next = h1;
-        }else{
-            cur->next = h2;
+        for(auto &n : next_arr){
+            cout << n << "->";
         }
-        return head->next;
+        cout << endl;
+        // next() arr initialize
+        // start search
+        int tar = 0; // pos in haystack 
+        int pos = 0; // pos in needle
+        while(tar < haystack.size()){
+            if(haystack[tar] == needle[pos]){
+                ++pos;
+                ++tar;
+            }else if(pos == 0){
+                ++tar;
+                // ++pos;
+            }else{
+                pos = next_arr[pos-1];
+            }
+            if(pos == needle.size()) return tar - pos;
+        }
+        return -1;
     }
 };
