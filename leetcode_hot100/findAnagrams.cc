7c0e5bacc61b2ac73509ad5d69f34c5efbbf0859
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2022-07-19 22:40:03 +0800

    Sliding windows

---
+++ leetcode_hot100/findAnagrams.cc
@@ -0,0 +1,45 @@
+#include "head.h"
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int sLen = s.size(), pLen = p.size();
+
+        if (sLen < pLen) {
+            return vector<int>();
+        }
+
+        vector<int> ans;
+        vector<int> sCount(26);
+        vector<int> pCount(26);
+        for (int i = 0; i < pLen; ++i) {
+            ++sCount[s[i] - 'a'];
+            ++pCount[p[i] - 'a'];
+        }
+
+        if (sCount == pCount) {
+            ans.emplace_back(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            --sCount[s[i] - 'a'];
+            ++sCount[s[i + pLen] - 'a'];
+
+            if (sCount == pCount) {
+                ans.emplace_back(i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+int main(){
+    Solution s;
+    string str("cbaebabacd");
+    string pattern("abc");
+    auto vec = s.findAnagrams(str, pattern);
+    printVector(vec);
+    return 0;
+}
