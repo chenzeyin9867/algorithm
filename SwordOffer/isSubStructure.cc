093b9c7544c2051342bcf3fd6d98b130bb561e10
chenzeyin9867 <chenzeyin9867@buaa.edu.cn>
2022-08-03 17:13:10 +0800

    could use dfs + kmp to speed up

---
+++ SwordOffer/isSubStructure.cc
@@ -0,0 +1,9 @@
+#include "basic_head.h"
+/** Decide whether B is a sub-structure of tree A. */
+class Solution {
+public:
+    bool isSubStructure(TreeNode* A, TreeNode* B) {
+        // DFS + KMP
+        
+    }
+};
